ab236d5c03763dcfe716756eb5b6868aeaf1d16d
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-10-26 22:34:13 +0200

    Cleanup.

--- src/fluid/pushbutton.cpp
+++ src/fluid/pushbutton.cpp
@@ -1,3 +1,26 @@
+/****************************************************************************
+ * This file is part of Widget Styles.
+ *
+ * Copyright (c) 2012 Pier Luigi Fiorini
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:LGPL-ONLY$
+ *
+ * This file may be used under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation and
+ * appearing in the file LICENSE.LGPL included in the packaging of
+ * this file, either version 2.1 of the License, or (at your option) any
+ * later version.  Please review the following information to ensure the
+ * GNU Lesser General Public License version 2.1 requirements
+ * will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * If you have questions regarding the use of this file, please contact
+ * us via http://www.maui-project.org/.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
 
 #include <QDebug>
 #include <QStyle>
@@ -11,15 +34,15 @@ using namespace Fluid;
 PushButton::PushButton(QObject *parent)
     : QObject(parent)
 {
-    m_background = new FrameSvg(this);
-    m_background->setImagePath("widgets/button");
-    m_background->setCacheAllRenderedFrames(true);
-    m_background->setElementPrefix("normal");
+    m_svgImage = new FrameSvg(this);
+    m_svgImage->setImagePath("widgets/button");
+    m_svgImage->setCacheAllRenderedFrames(true);
+    m_svgImage->setElementPrefix("normal");
 }
 
 PushButton::~PushButton()
 {
-    delete m_background;
+    delete m_svgImage;
 }
 
 void PushButton::paint(const QStyleOption *option, QPainter *painter, const QWidget *widget)
@@ -33,36 +56,36 @@ void PushButton::paint(const QStyleOption *option, QPainter *painter, const QWid
     //rect.adjust(m_margins.left(), m_margins.top(), -m_margins.right(), -m_margins.bottom());
     qDebug() << m_margins;
     rect.adjust(2, 2, -2, -2);
-    syncBorders(rect.size());
-    syncFrame(rect.size());
+    setBorders(rect.size());
+    setFrame(rect.size());
 
     QPixmap bufferPixmap;
 
     if (isEnabled) {
         // A normal button (pressed or not)
         if (isDown || isChecked)
-            m_background->setElementPrefix("pressed");
+            m_svgImage->setElementPrefix("pressed");
         else
-            m_background->setElementPrefix("normal");
+            m_svgImage->setElementPrefix("normal");
     } else if (!isEnabled || isFlat) {
         // A normal button disabled or a flat button
         bufferPixmap = QPixmap(rect.size());
         bufferPixmap.fill(Qt::transparent);
 
         QPainter bufferPainter(&bufferPixmap);
-        m_background->paintFrame(&bufferPainter);
+        m_svgImage->paintFrame(&bufferPainter);
         bufferPainter.setCompositionMode(QPainter::CompositionMode_DestinationIn);
         bufferPainter.fillRect(bufferPixmap.rect(), QColor(0, 0, 0, 128));
 
         painter->drawPixmap(0, 0, bufferPixmap);
     }
 
-    if (!isDown && !isChecked && isEnabled && m_background->hasElementPrefix("active")) {
+    if (!isDown && !isChecked && isEnabled && m_svgImage->hasElementPrefix("active")) {
         //....
-        m_background->setElementPrefix("active");
-        m_background->paintFrame(painter, m_activeRect.topLeft());
+        m_svgImage->setElementPrefix("active");
+        m_svgImage->paintFrame(painter, m_activeRect.topLeft());
     } else if (isEnabled) {
-        m_background->paintFrame(painter, QPointF(m_margins.left() + 1, m_margins.top() + 1));
+        m_svgImage->paintFrame(painter, QPointF(m_margins.left() + 1, m_margins.top() + 1));
     }
 }
 
@@ -70,61 +93,59 @@ void PushButton::drawDefaultFrame(const QStyleOption *option, QPainter *painter,
 {
     QRect rect(option->rect);
 
-    syncFrame(rect.size());
+    setFrame(rect.size());
 
-    m_background->setElementPrefix("focus");
-    m_background->paintFrame(painter);
+    m_svgImage->setElementPrefix("focus");
+    m_svgImage->paintFrame(painter);
 }
 
-void PushButton::syncFrame(QSize size)
+void PushButton::setFrame(QSize size)
 {
-    m_background->setElementPrefix("pressed");
-    m_background->resizeFrame(size);
+    m_svgImage->setElementPrefix("pressed");
+    m_svgImage->resizeFrame(size);
 
-    m_background->setElementPrefix("focus");
-    m_background->resizeFrame(size);
+    m_svgImage->setElementPrefix("focus");
+    m_svgImage->resizeFrame(size);
 
-    syncActiveRect(size);
+    setRect(size);
 
-    m_background->setElementPrefix("normal");
-    m_background->resizeFrame(size);
-    //hoverAnimation->setProperty("startPixmap", m_background->framePixmap());
+    m_svgImage->setElementPrefix("normal");
+    m_svgImage->resizeFrame(size);
 
-    m_background->setElementPrefix("active");
-    m_background->resizeFrame(m_activeRect.size());
-    //hoverAnimation->setProperty("targetPixmap", m_background->framePixmap());
+    m_svgImage->setElementPrefix("active");
+    m_svgImage->resizeFrame(m_activeRect.size());
 }
 
-void PushButton::syncActiveRect(QSize size)
+void PushButton::setRect(QSize size)
 {
-    m_background->setElementPrefix("normal");
+    m_svgImage->setElementPrefix("normal");
 
     qreal left, top, right, bottom;
-    m_background->getMargins(left, top, right, bottom);
+    m_svgImage->getMargins(left, top, right, bottom);
 
-    m_background->setElementPrefix("active");
+    m_svgImage->setElementPrefix("active");
     qreal activeLeft, activeTop, activeRight, activeBottom;
-    m_background->getMargins(activeLeft, activeTop, activeRight, activeBottom);
+    m_svgImage->getMargins(activeLeft, activeTop, activeRight, activeBottom);
 
     m_activeRect = QRectF(QPointF(0, 0), size);
     m_activeRect.adjust(left - activeLeft, top - activeTop,
                         -(right - activeRight), -(bottom - activeBottom));
 
-    m_background->setElementPrefix("normal");
+    m_svgImage->setElementPrefix("normal");
 }
 
-void PushButton::syncBorders(QSize size)
+void PushButton::setBorders(QSize size)
 {
     // Set margins from the normal element
     qreal left, top, right, bottom;
 
-    m_background->setElementPrefix("normal");
-    m_background->getMargins(left, top, right, bottom);
+    m_svgImage->setElementPrefix("normal");
+    m_svgImage->getMargins(left, top, right, bottom);
     left = top = right = bottom = 2;
     m_margins = QMargins(left, top, right, bottom);
 
     // Calculate the rect for the over effect
-    syncActiveRect(size);
+    setRect(size);
 }
 
 #include "moc_pushbutton.cpp"
--- src/fluid/pushbutton.h
+++ src/fluid/pushbutton.h
@@ -1,3 +1,26 @@
+/****************************************************************************
+ * This file is part of Widget Styles.
+ *
+ * Copyright (c) 2012 Pier Luigi Fiorini
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:LGPL-ONLY$
+ *
+ * This file may be used under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation and
+ * appearing in the file LICENSE.LGPL included in the packaging of
+ * this file, either version 2.1 of the License, or (at your option) any
+ * later version.  Please review the following information to ensure the
+ * GNU Lesser General Public License version 2.1 requirements
+ * will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ *
+ * If you have questions regarding the use of this file, please contact
+ * us via http://www.maui-project.org/.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
 
 #ifndef PUSHBUTTON_H
 #define PUSHBUTTON_H
@@ -15,13 +38,13 @@ public:
     void drawDefaultFrame(const QStyleOption *option, QPainter *painter, const QWidget *widget = 0);
 
 private:
-    Fluid::FrameSvg *m_background;
+    Fluid::FrameSvg *m_svgImage;
     QRectF m_activeRect;
     QMargins m_margins;
 
-    void syncFrame(QSize size);
-    void syncActiveRect(QSize size);
-    void syncBorders(QSize size);
+    void setFrame(QSize size);
+    void setRect(QSize size);
+    void setBorders(QSize size);
 };
 
 #endif // PUSHBUTTON_H
